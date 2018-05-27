9f1ec2f552382912d529428309de0bd58db0de2b
f.cerutti <f.cerutti@BLU05863.replynet.prv>
2018-05-27 19:00:08 +0200

    jenkins file

--- .jenkins/build.groovy
+++ .jenkins/build.groovy
@@ -1,25 +1,25 @@
 node {
-	def mvnHome
-	stage('Preparation') { // for display purposes
-		// Get some code from a GitHub repository
-		git 'https://github.com/fcerutti89/myFirstProject'
-		// Get the Maven tool.
-		// ** NOTE: This 'M3' Maven tool must be configured
-		// ** in the global configuration. 
-		mvnHome = tool 'M3'
-	}
-	stage('Build') {
-		// Run the maven build
-		if (isUnix()) {
-		sh "'${mvnHome}/bin/mvn' -Dmaven.test.failure.ignore clean package"
-		} else {
-		bat(/"${mvnHome}\bin\mvn" -Dmaven.test.failure.ignore clean package/)
-		}
-	}
-	stage('Docker build') {
-		docker build -f Dockerfile -t demo/spring:maven-3.3-jdk-8 .
-	}
-	stage('Upload dockerfile in repository') {
-		echo "upload"
-	}
+   def mvnHome
+   stage('Preparation') { // for display purposes
+      // Get some code from a GitHub repository
+      git 'https://github.com/fcerutti89/myFirstProject'
+      // Get the Maven tool.
+      // ** NOTE: This 'M3' Maven tool must be configured
+      // ** in the global configuration. 
+      mvnHome = tool 'M3'
+   }
+   stage('Build') {
+      // Run the maven build
+      if (isUnix()) {
+      sh "'${mvnHome}/bin/mvn' -Dmaven.test.failure.ignore clean package"
+      } else {
+      bat(/"${mvnHome}\bin\mvn" -Dmaven.test.failure.ignore clean package/)
+      }
+   }
+   stage('Docker build') {
+      docker build -f Dockerfile -t demo/spring:maven-3.3-jdk-8 .
+   }
+   stage('Upload dockerfile in repository') {
+      echo "upload"
+   }
 }
