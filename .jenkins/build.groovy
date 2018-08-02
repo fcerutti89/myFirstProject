134b120f6584c7fe345aee016fff8a3995d048f3
f.cerutti <f.cerutti@BLU05863.replynet.prv>
2018-08-02 17:00:02 +0200

    fixed jenkins pipeline config

--- .jenkins/build.groovy
+++ .jenkins/build.groovy
@@ -13,6 +13,7 @@ node {
       // requires SonarQube Scanner for Maven 3.2+
       sh 'mvn org.sonarsource.scanner.maven:sonar-maven-plugin:3.2:sonar'
     }
+   }
    stage('Build') {
       // Run the maven build
       if (isUnix()) {
