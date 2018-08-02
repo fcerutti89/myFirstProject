6deaa69e1ec21ec7b8a3739097fc1972c2715691
f.cerutti <f.cerutti@BLU05863.replynet.prv>
2018-08-02 16:58:13 +0200

    added sonarqube stage on jenkins pipeline

--- .jenkins/build.groovy
+++ .jenkins/build.groovy
@@ -8,6 +8,11 @@ node {
       // ** in the global configuration. 
       mvnHome = tool 'M3'
    }
+   stage('SonarQube analysis') {
+    withSonarQubeEnv('sonarqube72') {
+      // requires SonarQube Scanner for Maven 3.2+
+      sh 'mvn org.sonarsource.scanner.maven:sonar-maven-plugin:3.2:sonar'
+    }
    stage('Build') {
       // Run the maven build
       if (isUnix()) {
