66eefb27e0dc6c93a295b0df5c24ec3c0fb41046
fcerutti89 <38767664+fcerutti89@users.noreply.github.com>
2018-10-12 17:04:19 +0200

    Update build.groovy

--- .jenkins/build.groovy
+++ .jenkins/build.groovy
@@ -8,12 +8,12 @@ node {
       // ** in the global configuration. 
       mvnHome = tool 'M3'
    }
-   stage('SonarQube analysis') {
-    withSonarQubeEnv('sonarqube72') {
-      // requires SonarQube Scanner for Maven 3.2+
-      sh 'mvn org.sonarsource.scanner.maven:sonar-maven-plugin:3.2:sonar'
-    }
-   }
+   //stage('SonarQube analysis') {
+   // withSonarQubeEnv('sonarqube72') {
+   //   // requires SonarQube Scanner for Maven 3.2+
+   //   sh 'mvn org.sonarsource.scanner.maven:sonar-maven-plugin:3.2:sonar'
+   // }
+   //}
    stage('Build') {
       // Run the maven build
       if (isUnix()) {
